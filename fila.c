67267d045459c7f43e5f257ab62cb7d7ad326044
joaocabral6 <38953911+joaocabral6@users.noreply.github.com>
2018-05-03 11:54:16 -0400

    fila.c
    Aqui vai ficar as funções

---
+++ fila.c
@@ -0,0 +1,23 @@
+aviao *inserir(aviao **inicio, aviao **fim, aviao *novo){
+
+}
+
+aviao *remover(aviao **inicio){
+
+}
+
+void *contar(aviao **inicio, aviao **fim){
+
+}
+
+void *imprimir(aviao **inicio, aviao **fim){
+
+}
+
+void *consultar_cabeça(aviao **inicio){
+
+}
+
+void *quantidadeDePessoas(aviao **inicio, aviao **fim){
+
+}
